e8956d8a004116b78409ed5f721fd04ee4848ec2
augustinho2 <augusto_guimaraes@gec.inatel.br>
2023-09-19 23:00:43 -0300

    summarizing the text and showing the resume, application completed

--- server/summarize.js
+++ server/summarize.js
@@ -1,5 +1,22 @@
 import { summaryExample } from './utils/summary.js'
+import { pipeline } from '@xenova/transformers'
 
 export async function summarize(text){
-    return summaryExample
+   // return summaryExample
+   try {
+    console.log('Realizando o resumo...')
+
+    const generator = await pipeline(
+        'summarization',
+        'Xenova/distilbart-cnn-12-6'
+    )
+
+    const output = await generator(text)
+
+    console.log("resumo concluido!")
+    return output[0].summary_text
+   } catch (error) {
+    console.log('nao foi possivel realizar o resumo', error)
+    throw new Error(error)
+   }
 }
